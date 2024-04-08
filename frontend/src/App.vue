1d0cb2a1c8976f20dc12428664ef7e4f58ae4a30
PierreJack <pierrejacques2002@gmail.com>
2024-04-08 19:03:24 +0200

    added missing modules

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,123 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1> Signal Flow Graph</h1>
+    <div class="tool-wrapper">
+      <select v-model="newNodeType">
+        <option v-for="(item, index) in nodeCategory" :key="index" :value="index">{{item}}</option>
+      </select>
+      <input type="text" v-model="newNodeLabel" placeholder="Input node label">
+      <button @click="addNode">ADD</button>
+    </div>
+    
+    <simple-flowchart :scene.sync="scene" 
+      @nodeClick="nodeClick"
+      @nodeDelete="nodeDelete"
+      @linkBreak="linkBreak"
+      @linkAdded="linkAdded"
+      @canvasClick="canvasClick"
+      :height="800"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SimpleFlowchart from './components/SimpleFlowchart.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    SimpleFlowchart
+  },
+  data() {
+    return {
+      scene: {
+        centerX: 1024,
+        centerY: 140,
+        scale: 1,
+        nodes: [
+          {
+            id: 1,
+            x: -700,
+            y: -69,
+            type: 'Q',
+            label: '1',
+          },
+          {
+            id: 2,
+            x: -357,
+            y: 80,
+            type: 'M',
+            label: '2',
+          },
+          {
+            id: 3,
+            x: -557,
+            y: 80,
+            type: 'Q',
+            label: '3',
+          }
+        ],
+        links: [
+          {
+            id: 1,
+            from: 3, // node id the link start
+            to: 2,  // node id the link end
+            weight: 1,
+            
+          }
+        ]
+      },
+      newNodeType: 0,
+      newNodeLabel: '',
+      nodeCategory:[
+        'M',
+        'Q'
+      ],
+    }
+  },
+  methods: {
+    canvasClick(e) {
+      console.log('canvas Click, event:', e)
+    },
+    addNode() {
+      let maxID = Math.max(0, ...this.scene.nodes.map((link) => {
+        return link.id
+      }))
+      this.scene.nodes.push({
+        id: maxID + 1,
+        x: -400,
+        y: 50,
+        type: this.nodeCategory[this.newNodeType],
+        label: this.newNodeLabel ? this.newNodeLabel: `test${maxID + 1}`,
+      })
+    },
+    nodeClick(id) {
+      console.log('node click', id);
+    },
+    nodeDelete(id) {
+      console.log('node delete', id);
+    },
+    linkBreak(id) {
+      console.log('link break', id);
+    },
+    linkAdded(link) {
+      console.log('new link added:', link);
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  overflow: hidden;
+  height: 800px;
+  .tool-wrapper {
+    position: relative;
+  }
 }
 </style>
---
+++ frontend/src/components/FlowchartNode.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="flowchart-node" :style="nodeStyle" 
+    @mousedown="handleMousedown"
+    @mouseover="handleMouseOver"
+    @mouseleave="handleMouseLeave"
+    v-bind:class="{selected: options.selected === id}">
+    <div class="node-port node-input"
+       @mousedown="inputMouseDown"
+       @mouseup="inputMouseUp">
+    </div>
+    <div class="node-main">
+      <!-- <div v-text="type" class="node-type"></div> -->
+      <div v-text="id" class="node-label"></div>
+    </div>
+    <div class="node-port node-output" 
+      @mousedown="outputMouseDown">
+    </div>
+    <!-- <div v-show="show.delete" class="node-delete">&times;</div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FlowchartNode',
+  props: {
+    id: {
+      type: Number,
+      default: 1000,
+      validator(val) {
+        return typeof val === 'number'
+      }
+    },
+    x: {
+      type: Number,
+      default: 0,
+      validator(val) {
+        return typeof val === 'number'
+      }
+    },    
+    y: {
+      type: Number,
+      default: 0,
+      validator(val) {
+        return typeof val === 'number'
+      }
+    },
+    type: {
+      type: String,
+      default: 'Default'
+    },
+    label: {
+      type: String,
+      default: 'input name'
+    },
+    options: {
+      type: Object,
+      default() {
+        return {
+          centerX: 1024,
+          scale: 1,
+          centerY: 140,
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      show: {
+        delete: false,
+      }
+    }
+  },
+  mounted() {
+  },
+  computed: {
+    nodeStyle() {
+      return {
+        top: this.options.centerY + this.y * this.options.scale + 'px', // remove: this.options.offsetTop + 
+        left: this.options.centerX + this.x * this.options.scale + 'px', // remove: this.options.offsetLeft + 
+        transform: `scale(${this.options.scale})`,
+      }
+    }
+  },
+  methods: {
+    handleMousedown(e) {
+      const target = e.target || e.srcElement;
+      // console.log(target);
+      if (target.className.indexOf('node-input') < 0 && target.className.indexOf('node-output') < 0) {
+        this.$emit('nodeSelected', e);
+      }
+      e.preventDefault();
+    },
+    handleMouseOver() {
+      this.show.delete = true;
+    },
+    handleMouseLeave() {
+      this.show.delete = false;
+    },
+    outputMouseDown(e) {
+      this.$emit('linkingStart')
+      e.preventDefault();
+    },
+    inputMouseDown(e) {
+      e.preventDefault();
+    },
+    inputMouseUp(e) {
+      this.$emit('linkingStop')
+      e.preventDefault();
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+$themeColor: rgb(255, 136, 85);
+$portSize: 12;
+
+.flowchart-node {
+  border-radius: 50%;
+  margin: 0;
+  width: 80px;
+  height: 80px;
+  position: absolute;
+  box-sizing: border-box;
+  border: none;
+  background: white;
+  z-index: 1;
+  opacity: .9;
+  cursor: move;
+  transform-origin: top left;
+  .node-main {
+    text-align: center;
+    .node-type {
+      background: $themeColor;
+      color: white;
+      font-size: 13px;
+      padding: 6px;
+    }
+    .node-label {
+      font-size: 68px;
+      //text-align: center;
+      color: rgba(255, 136, 85, 0.8);
+    }
+  }
+  .node-port {
+    position: absolute;
+    width: #{$portSize}px;
+    height: #{$portSize}px;
+    left: 50%;
+    transform: translate(-50%);
+    border: 1px solid #ccc;
+    //border-radius: 100px;
+    background: white;
+    &:hover {
+      background: $themeColor;
+      border: 1px solid $themeColor;
+    }
+  }
+  .node-input {
+    top: #{-2+$portSize/-2 + 40}px;
+    left: 0;
+  }
+  .node-output {
+    bottom: #{-2+$portSize/-2 + 40}px;
+    left: 80px;
+  }
+  .node-delete {
+    position: absolute;
+    right: -6px;
+    top: -6px;
+    font-size: 12px;
+    width: 12px;
+    height: 12px;
+    color: $themeColor;
+    cursor: pointer;
+    background: white;
+    border: 1px solid $themeColor;
+    border-radius: 100px;
+    text-align: center;
+    &:hover{
+      background: $themeColor;
+      color: white;
+    }
+  }
+}
+.selected {
+  box-shadow: 0 0 0 2px $themeColor;
+}
+</style>
