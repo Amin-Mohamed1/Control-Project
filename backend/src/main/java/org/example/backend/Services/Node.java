7226748c412ce63dd16b5e3af2c6e90bcf6acd2e
Amen Mohamed <145727573+WorldisAmen@users.noreply.github.com>
2024-04-14 19:06:46 +0200

    updated node class

--- backend/src/main/java/org/example/backend/Services/Node.java
+++ backend/src/main/java/org/example/backend/Services/Node.java
@@ -1,11 +1,32 @@
 package org.example.backend.Services;
-import java.util.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
-    String name;
-    List<Edge> edges;
+    private final String name;
+    private final List<Edge> edges;
 
     public Node(String name) {
         this.name = name;
-        edges = new ArrayList<>();
+        this.edges = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+    public void addEdge(Node destination, double gain) {
+        edges.add(new Edge(destination, gain));
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
